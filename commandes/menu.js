123324ad54215014d927bb568c1d042dbe4e2556
Fredi AI <frediezra60@gmail.com>
2025-12-31 14:03:06 +0300

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -1,86 +1,113 @@
-const { zokou } = require(__dirname + "/../framework/zokou");
-const { format } = require(__dirname + "/../framework/mesfonctions");
-const os = require("os");
+"use strict";
+const { zokou } = require("../framework/zokou");
 const moment = require("moment-timezone");
-const s = require(__dirname + "/../set");
+const os = require("os");
+const s = require("../set");
 
-zokou({ nomCom: "timoth", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre, prefixe, mybotpic } = commandeOptions;
+const readMore = String.fromCharCode(8206).repeat(4001);
+
+// Function to convert text to fancy uppercase font
+const toFancyUppercaseFont = (text) => {
+    const fonts = {
+        'A': '𝐀', 'B': '𝐁', 'C': '𝐂', 'D': '𝐃', 'E': '𝐄', 'F': '𝐅', 'G': '𝐆', 'H': '𝐇', 'I': '𝐈', 'J': '𝐉', 'K': '𝐊', 'L': '𝐋', 'M': '𝐌',
+        'N': '𝐍', 'O': '𝐎', 'P': '𝐏', 'Q': '𝐐', 'R': '𝐑', 'S': '𝐒', 'T': '𝐓', 'U': '𝐔', 'V': '𝐕', 'W': '𝐖', 'X': '𝐗', 'Y': '𝐘', 'Z': '𝐙'
+    };
+    return text.split('').map(char => fonts[char] || char).join('');
+};
+
+// Function to convert text to fancy lowercase font
+const toFancyLowercaseFont = (text) => {
+    const fonts = {
+        'a': 'ᴀ', 'b': 'ʙ', 'c': 'ᴄ', 'd': 'ᴅ', 'e': 'ᴇ', 'f': 'ғ', 'g': 'ɢ', 'h': 'ʜ', 'i': 'ɪ', 'j': 'ᴊ', 'k': 'ᴋ', 'l': 'ʟ', 'm': 'ᴍ',
+        'n': 'ɴ', 'o': 'ᴏ', 'p': 'ᴘ', 'q': 'ǫ', 'r': 'ʀ', 's': 's', 't': 'ᴛ', 'u': 'ᴜ', 'v': 'ᴠ', 'w': 'ᴡ', 'x': 'x', 'y': 'ʏ', 'z': 'ᴢ'
+    };
+    return text.split('').map(char => fonts[char] || char).join('');
+};
+
+zokou({ 
+    nomCom: "menu", 
+    categorie: "Fredi-Menu", 
+    reaction: "☢️", 
+    nomFichier: __filename 
+}, async (dest, zk, commandeOptions) => {
+    const { repondre, prefixe, nomAuteurMessage } = commandeOptions;
+    const { cm } = require("../framework/zokou");
+    let coms = {};
+    let mode = "public";
     
-    // Muhimu: Tunaita 'cm' moja kwa moja kutoka kwenye framework
-    const { cm } = require(__dirname + "/../framework/zokou");
-    
-    var coms = {};
-    var mode = (s.MODE).toLocaleLowerCase() != "yes" ? "Private" : "Public";
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
 
-    // 1. Kuhesabu Idadi ya Commands
-    let totalCommands = cm.length;
-
-    cm.map((com) => {
+    cm.map(async (com) => {
         if (!coms[com.categorie]) coms[com.categorie] = [];
         coms[com.categorie].push(com.nomCom);
     });
 
-    // 2. Muda wa Tanzania
-    moment.tz.setDefault('Africa/Dar_es_Salaam');
-    const hour = moment().format('HH');
+    moment.tz.setDefault("Africa/Dar_es_Salaam");
+    const hour = moment().hour();
+    let greeting = "ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ";
+    if (hour >= 12 && hour < 18) greeting = "ɢᴏᴏᴅ ᴀғᴛᴇʀɴᴏᴏɴ!";
+    else if (hour >= 18) greeting = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ!";
+    else if (hour >= 22 || hour < 5) greeting = "ɢᴏᴏᴅ ɴɪɢʜᴛ";
+
     const temps = moment().format('HH:mm:ss');
     const date = moment().format('DD/MM/YYYY');
+    const img = 'https://files.catbox.moe/3o37c5.jpeg';
+    const imgs = 'https://files.catbox.moe/uw4l17.jpeg';
 
-    // 3. Salamu na Picha
-    let greeting = "";
-    let myPictures = [];
-
-    if (hour < 12) {
-        greeting = "🌅 GOOD MORNING";
-        myPictures = ["https://files.catbox.moe/p8mv62.jpg", "https://files.catbox.moe/p8mv62.jpg"]; // Weka URL 6 hapa
-    } else if (hour < 18) {
-        greeting = "☀️ GOOD AFTERNOON";
-        myPictures = ["https://files.catbox.moe/p8mv62.jpg", "https://files.catbox.moe/p8mv62.jpg"];
-    } else {
-        greeting = "🌃 GOOD EVENING";
-        myPictures = ["https://files.catbox.moe/p8mv62.jpg", "https://files.catbox.moe/p8mv62.jpg"];
-    }
-
-    const randomPic = myPictures[Math.floor(Math.random() * myPictures.length)];
-
-    let menuMsg = `
-╔═══════『 **𝚻𝚰𝚳𝚴𝚫𝐒𝚫 𝚻𝚳𝐃2** 』═══════╗
-┃
-┃  👋 **${greeting}**
-┃  👤 **USER**: ${s.OWNER_NAME}
-┃  🕒 **TIME**: ${temps}
-┃  📅 **DATE**: ${date}
-┃  📊 **COMMANDS**: ${totalCommands}
-┃  ⚙️ **MODE**: ${mode}
-┃  🔋 **RAM**: ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
-┃
-╚═════════════════════════════╝\n\n`;
-
+    const infoMsg = `
+╭┈┈┈┈━⊷
+*┋* *ʙᴏᴛ ɴᴀᴍᴇ :*  *☢️LUCKY MD X-FORCE☢️*
+*┋* *ᴘʀᴇғɪx :* [ ${s.PREFIXE} ]
+*┋* *ᴍᴏᴅᴇ :* ${mode}
+*┋* *ᴅᴀᴛᴇ  :* ${date}
+*┋* *ᴘʟᴀᴛғᴏʀᴍ :* ${os.platform()}
+*┋* *ᴏᴡɴᴇʀ ɪs : FREDI*
+*┋* *ᴘʟᴜɢɪɴs ᴄᴍᴅ :* ${cm.length}
+╰┈┈┈┈━⊷\n`;
+    
+    let menuMsg = ` *${greeting}*`;
+    
     for (const cat in coms) {
-        menuMsg += `✨ *${cat.toUpperCase()}* ✨\n`;
+        menuMsg += `
+*「 ${toFancyUppercaseFont(cat)} 」*
+╭─━⊷ `;
         for (const cmd of coms[cat]) {
-            menuMsg += `  ◦ ${cmd}\n`;
+            menuMsg += `          
+*┋* ${toFancyLowercaseFont(cmd)}`;   
         }
-        menuMsg += `\n`;
+        menuMsg += `
+╰─━⊷`;
     }
+    
+    menuMsg += `
+> @made by FredieTech 2025\n`;
 
     try {
-        await zk.sendMessage(dest, {
-            image: { url: randomPic },
-            caption: menuMsg,
+        await zk.sendMessage(dest, { 
+            image: { url: img },
+            caption: infoMsg + menuMsg,
             contextInfo: {
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: "120363313124070136@newsletter",
+                    newsletterName: "@FrediEzra",
+                    serverMessageId: -1
+                },
+                forwardingScore: 999,
                 externalAdReply: {
-                    title: "TIMNASA TMD2 MENU",
-                    body: "Powered by Timnasa",
-                    thumbnailUrl: randomPic,
-                    sourceUrl: "https://whatsapp.com/channel/120363413554978773@newsletter",
+                    title: "☢️LUCKY MD X-FORCE☢️",
+                    body: "🗡️FredieTech Commands List",
+                    thumbnailUrl: imgs,
+                    sourceUrl: "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f",
                     mediaType: 1,
                     renderLargerThumbnail: true
                 }
             }
-        }, { quoted: ms });
-    } catch (e) {
-        repondre("Error: " + e.message);
+        });
+      } catch (error) {
+        console.error("Menu error: ", error);
+        repondre("🥵🥵 Menu error: " + error);
     }
 });
